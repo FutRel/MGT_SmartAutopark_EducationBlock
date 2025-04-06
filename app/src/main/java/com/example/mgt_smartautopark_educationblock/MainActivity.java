fb436e0ed3954b78224aeb61882ec9afe575209a
FutRel <gxrim@bk.ru>
2025-04-06 16:33:32 +0300

    authorization view visual created

--- app/src/main/java/com/example/mgt_smartautopark_educationblock/MainActivity.java
+++ app/src/main/java/com/example/mgt_smartautopark_educationblock/MainActivity.java
@@ -5,10 +5,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
